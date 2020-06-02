4b9f2ff120d5f3cb0a858b094b6a9cc8064ce617
EdwinM96 <edwin.morales9b@gmail.com>
2020-06-02 11:53:36 -0600

    Added JPAConfiguration class

---
+++ src/main/java/com/fuimonos/configuration/JPAConfiguration.java
@@ -0,0 +1,60 @@
+package com.fuimonos.configuration;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.christmascards.repository"})
+public class JPAConfiguration {
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource((javax.sql.DataSource) dataSource());
+		em.setPersistenceUnitName("persistenceunit");
+		em.setPackagesToScan("com.christmascards.domain");
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		Properties jpaProperties = hibernateProperties();
+		em.setJpaProperties(jpaProperties);
+		return em;
+	}
+	
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.postgresql.Driver");
+		dataSource.setUrl(System.getenv("FUIMONOS_URL"));
+		dataSource.setUsername(System.getenv("FUIMONOS_USERNAME"));
+		dataSource.setPassword(System.getenv("FUIMONOS_PASSWORD"));
+		return (DataSource) dataSource;
+	}
+	
+	Properties hibernateProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("hibernate.show_sql","true");
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");            
+		return properties;
+	}
+}
