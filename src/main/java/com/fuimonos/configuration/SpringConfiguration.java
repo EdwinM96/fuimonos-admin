7dc5c01ae7a8f95eb89d2f95d2dbf0bc089c56bc
EdwinM96 <edwin.morales9b@gmail.com>
2020-06-02 11:44:53 -0600

    Initialization

---
+++ src/main/java/com/fuimonos/configuration/SpringConfiguration.java
@@ -0,0 +1,31 @@
+package com.fuimonos.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.christmascards")
+public class SpringConfiguration implements WebMvcConfigurer {
+
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+        
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry){
+            registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
+        }
+}
---
+++ src/main/java/com/fuimonos/configuration/SpringInitializer.java
@@ -0,0 +1,31 @@
+package com.fuimonos.configuration;
+
+import java.util.TimerTask;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringInitializer implements WebApplicationInitializer{
+    
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		
+		ctx.register(SpringConfiguration.class);
+		ctx.setServletContext(container);
+		
+		ServletRegistration.Dynamic servlet = container.addServlet("christmas-cards", new DispatcherServlet(ctx));
+		
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+		
+                
+	}
+	
+}
